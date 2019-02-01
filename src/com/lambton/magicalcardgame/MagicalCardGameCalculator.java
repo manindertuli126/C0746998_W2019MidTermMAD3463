5397db9ab9288a7365d5e3083cf6d08b66b3d12c
manindertuli126 <maninder.tuli126@gmail.com>
2019-01-31 20:32:22 -0500

    Added code to shuffle for first column .

--- src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
@@ -5,19 +5,24 @@ public class MagicalCardGameCalculator {
         int rowNumber = 0;
         int columnNumber = 0;
         String [][] cards1 = magicalCardGameModel.getCardList();
+        String tempArray [][]=new String[2][2];
         String firstShuffleColPos = magicalCardGameModel.firstShuffleColPos;
         columnNumber = Integer.parseInt(firstShuffleColPos.substring(1,2));
         if(firstShuffleColPos.contains("A")){
             rowNumber =0;
+            for(int y=0; y<cards1.length;y++){
+                for (int u=0;u<cards1.length;u++){
+                    tempArray[y][u]=cards1[y+1][u+1];
+                }
+            }
         }else if (firstShuffleColPos.contains("B")){
             rowNumber =1;
         }else if (firstShuffleColPos.contains("C")) {
             rowNumber = 2;
         }
-
         return cards1;
     }
-//
+
 //    public String[][] getSecShuffleResult(MagicalCardGameModel magicalCardGameModel){
 //
 //    }
--- src/com/lambton/magicalcardgame/MagicalCardGameMain.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameMain.java
@@ -10,18 +10,27 @@ public class MagicalCardGameMain{
         System.out.println("Welcome to Magical Card Game ..");
         String [][]cards = {{"1","A","5"},{"2","7","3"},{"3","6","K"}};
         System.out.println("Please select one card from below");
-        for (int i=0;i<cards.length;i++){
-            for (int j=0;j<cards.length;j++){
-               System.out.print(cards[i][j]);
+        for (int i=0;i<cards.length;i++) {
+            for (int j = 0; j < cards.length; j++) {
+                System.out.print(cards[i][j]);
                 System.out.print(" , ");
-                }
+            }
             System.out.println(" ");
         }
-
-
         magicalCardGameModel.setCardList(cards);
         Scanner scanner = new Scanner(System. in);
+        System.out.println("Please tell the column in which your card falls");
         String FirstShuffleColPos = scanner. nextLine();
+        System.out.println("Column selected: "+FirstShuffleColPos);
         magicalCardGameModel.setFirstShuffleColPos(FirstShuffleColPos);
+        MagicalCardGameCalculator magicalCardGameCalculator = new MagicalCardGameCalculator();
+//        magicalCardGameModel.setCardList()=;
+        for (int i=0;i<cards.length;i++) {
+            for (int j = 0; j < cards.length; j++) {
+                System.out.print(cards[i][j]);
+                System.out.print(" , ");
+            }
+            System.out.println(" ");
+        }
         }
 }
