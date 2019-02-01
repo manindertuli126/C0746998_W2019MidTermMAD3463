2b147b633a9fe8526c07bfe1ca5037e83309658f
manindertuli126 <maninder.tuli126@gmail.com>
2019-01-31 19:42:06 -0500

    Added code in main class

--- src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
@@ -2,8 +2,18 @@ package com.lambton.magicalcardgame;
 
 public class MagicalCardGameCalculator {
     public String[][] getFirstShuffleResult(MagicalCardGameModel magicalCardGameModel){
+        int rowNumber = 0;
+        int columnNumber = 0;
         String [][] cards1 = magicalCardGameModel.getCardList();
-        if(magicalCardGameModel.firstShuffleColPos.contains("A1")){}
+        if(magicalCardGameModel.firstShuffleColPos.contains("A")){
+            rowNumber =0;
+
+        }else if (magicalCardGameModel.firstShuffleColPos.contains("B")){
+            rowNumber =1;
+        }else if (magicalCardGameModel.firstShuffleColPos.contains("C")){
+            rowNumber =2;
+        }
+
 
         return cards1;
     }
--- src/com/lambton/magicalcardgame/MagicalCardGameMain.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameMain.java
@@ -1,5 +1,7 @@
 package com.lambton.magicalcardgame;
 
+import java.util.Scanner;
+
 public class MagicalCardGameMain{
 
     public static void main (String args[])
@@ -7,6 +9,8 @@ public class MagicalCardGameMain{
         MagicalCardGameModel magicalCardGameModel = new MagicalCardGameModel();
         String [][]cards = {{"1","A","5"},{"2","7","3"},{"3","6","K"}};
         magicalCardGameModel.setCardList(cards);
-
+        Scanner scanner = new Scanner(System. in);
+        String FirstShuffleColPos = scanner. nextLine();
+        magicalCardGameModel.setFirstShuffleColPos(FirstShuffleColPos);
         }
 }
