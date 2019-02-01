b12b5982be09b5c74d9ba1079a5df0488201e6dd
manindertuli126 <maninder.tuli126@gmail.com>
2019-01-31 19:52:20 -0500

    code added to decoded the firstShuffleColPos to get row and column.

--- src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
@@ -5,12 +5,13 @@ public class MagicalCardGameCalculator {
         int rowNumber = 0;
         int columnNumber = 0;
         String [][] cards1 = magicalCardGameModel.getCardList();
-        if(magicalCardGameModel.firstShuffleColPos.contains("A")){
+        String firstShuffleColPos = magicalCardGameModel.firstShuffleColPos;
+        columnNumber = Integer.parseInt(firstShuffleColPos.substring(1,2));
+        if(firstShuffleColPos.contains("A")){
             rowNumber =0;
-
-        }else if (magicalCardGameModel.firstShuffleColPos.contains("B")){
+        }else if (firstShuffleColPos.contains("B")){
             rowNumber =1;
-        }else if (magicalCardGameModel.firstShuffleColPos.contains("C")){
+        }else if (firstShuffleColPos.contains("C")){
             rowNumber =2;
         }
 
