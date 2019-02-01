9f9648acd3e62c18642f22099b5bdbd77ece5d74
manindertuli126 <maninder.tuli126@gmail.com>
2019-01-31 20:03:44 -0500

    Added code to main class to display arrary.

--- src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
@@ -11,18 +11,17 @@ public class MagicalCardGameCalculator {
             rowNumber =0;
         }else if (firstShuffleColPos.contains("B")){
             rowNumber =1;
-        }else if (firstShuffleColPos.contains("C")){
-            rowNumber =2;
+        }else if (firstShuffleColPos.contains("C")) {
+            rowNumber = 2;
         }
-
 
         return cards1;
     }
-
-    public String[][] getSecShuffleResult(MagicalCardGameModel magicalCardGameModel){
-
-    }
-    public String getFinalResult(MagicalCardGameModel magicalCardGameModel){
-
-    }
+//
+//    public String[][] getSecShuffleResult(MagicalCardGameModel magicalCardGameModel){
+//
+//    }
+//    public String getFinalResult(MagicalCardGameModel magicalCardGameModel){
+//
+//    }
 }
--- src/com/lambton/magicalcardgame/MagicalCardGameMain.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameMain.java
@@ -7,7 +7,18 @@ public class MagicalCardGameMain{
     public static void main (String args[])
     {
         MagicalCardGameModel magicalCardGameModel = new MagicalCardGameModel();
+        System.out.println("Welcome to Magical Card Game ..");
         String [][]cards = {{"1","A","5"},{"2","7","3"},{"3","6","K"}};
+        System.out.println("Please select one card from below");
+        for (int i=0;i<cards.length;i++){
+            for (int j=0;j<cards.length;j++){
+               System.out.print(cards[i][j]);
+                System.out.print(" , ");
+                }
+            System.out.println(" ");
+        }
+
+
         magicalCardGameModel.setCardList(cards);
         Scanner scanner = new Scanner(System. in);
         String FirstShuffleColPos = scanner. nextLine();
